b27939f95bdf3841244748e3c3e5f201e866ca5f
ringodev <ringodev@protonmail.com>
2020-06-02 14:56:52 +0200

    final commit

---
+++ src/blackjack/ui/App.java
@@ -0,0 +1,140 @@
+package blackjack.ui;
+
+import blackjack.model.*;
+import blackjack.model.Event;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Application class for the graphical Blackjack game.
+ * Sets up the frame and starts the application.
+ */
+public class App {
+    private final Game model;
+    private final ViewPanel view;
+    private final JLabel stateLbl;
+
+
+    /**
+     * Main method starting the application.
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        new App(new Blackjack());
+    }
+
+    /**
+     * Constructor initializing the model and setting up the application.
+     *
+     * @param model holds all the game data while the application is running.
+     */
+    public App(Game model) {
+        this.model = model;
+        Listener gameListener = new Listener() {
+            @Override
+            public void update(Event e) {
+                System.out.println("updated");
+                setLabelText(e);
+            }
+        };
+
+        model.addGameListener(gameListener);
+
+        JFrame frame = new JFrame("Blackjack");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setJMenuBar(createMenu());
+
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new BorderLayout(10, 10));
+
+        this.view = new ViewPanel(model);
+        contentPane.add(view, BorderLayout.CENTER);
+
+        JPanel statePanel = new JPanel();
+        this.stateLbl = new JLabel(preGameMsg());
+        statePanel.setLayout(new FlowLayout());
+        statePanel.add(stateLbl);
+        contentPane.add(statePanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Resets the game
+     */
+    public void newGame() {
+        model.FullReset();
+    }
+
+    /**
+     * Creates the menu for this application
+     *
+     * @return the menu bar
+     */
+    private JMenuBar createMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem newItem = new JMenuItem("Reset", 'R');
+        newItem.addActionListener(e -> newGame());
+        JMenuItem exitItem = new JMenuItem("Exit", 'X');
+        exitItem.addActionListener(e -> System.exit(0));
+        menu.add(newItem);
+        menu.add(exitItem);
+        menuBar.add(menu);
+        return menuBar;
+    }
+
+
+    /**
+     * sets the correct Text of the bottom label
+     */
+    void setLabelText(Event e) {
+        switch (e.getState()) {
+            case NOTSTARTED -> stateLbl.setText(preGameMsg());
+            case RUNNING -> stateLbl.setText(inGameMsg());
+            case FINISHED -> stateLbl.setText(postGameMsg());
+        }
+    }
+
+    /**
+     * constructs the after game message with result of the last game
+     *
+     * @return the after game message
+     */
+    private String postGameMsg() {
+        StringBuilder sb = new StringBuilder();
+        switch (this.model.evaluateCards()) {
+            case DEALER_WINS:
+                sb.append("You just lost ").append(+this.model.getHuman().getLastWager()).append(this.model.getHuman().getLastWager() > 1 ? " Chips. " : " Chip. ");
+                break;
+            case PLAYER_WINS:
+                if (this.model.getHuman().hasBlackJack())
+                    sb.append("You made a Blackjack and won ").append(this.model.getHuman().getLastWager() * 2).append(this.model.getHuman().getLastWager() * 2 > 1 ? " Chips. " : " Chip. ");
+                else
+                    sb.append("You just won ").append(this.model.getHuman().getLastWager()).append(this.model.getHuman().getLastWager() > 1 ? " Chips. " : " Chip. ");
+                break;
+            case DRAW:
+                sb.append("This one was a draw. ");
+                break;
+        }
+        return sb.append("Click anywhere to try again!").toString();
+    }
+
+    /**
+     * @return the in game message
+     */
+    private String inGameMsg() {
+        return "Chips:" + model.getHuman().getChips() + "        Current Wager: " + model.getHuman().getWager();
+    }
+
+    /**
+     * @return the pre game message
+     */
+    private String preGameMsg() {
+        return "Press Play to start the Game";
+    }
+}
---
+++ src/blackjack/ui/ViewPanel.java
@@ -0,0 +1,167 @@
+package blackjack.ui;
+
+import blackjack.model.*;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ViewPanel extends JPanel {
+
+    /**
+     * The reference to the model.
+     */
+    private final Game model;
+
+    private Controller mouseListener;
+
+    private JPanel buttonPanel;
+    private JComponent dealerCardPanel;
+    private JComponent humanCardPanel;
+    private JPanel playPanel;
+    private JLabel dealerScore;
+    private JLabel humanScore;
+
+    private JButton doubleDown;
+    private JButton hit;
+    private JButton stay;
+    private JButton play;
+
+    /**
+     * Constructor initializing the model and setting up this view.
+     *
+     * @param model the model of the blackjack game.
+     */
+    public ViewPanel(Game model) {
+
+        super();
+        this.model = model;
+        this.setPreferredSize(new Dimension(600, 600));
+
+        this.model.addGameListener(e -> {
+            repaint();
+            updateScore();
+            if (e.getState() == GameState.NOTSTARTED) {
+
+                //exchanges playButton visibility with TurnButtons visibility
+                playPanel.setVisible(true);
+                buttonPanel.setVisible(false);
+            }
+            if (e.getState() == GameState.RUNNING) {
+
+                // removes the mouseListener from the view
+                removeMouseListener(mouseListener);
+
+                //exchanges playButton visibility with TurnButtons visibility
+                playPanel.setVisible(false);
+                buttonPanel.setVisible(true);
+
+                // makes sure the DoubleDown button is visible
+                doubleDown.setVisible(true);
+
+            }
+            if (e.getState() == GameState.FINISHED) {
+
+                // adds the mouseListener to the view
+                addMouseListener(mouseListener);
+
+                // makes hit stay and doubleDown buttons invisible
+                buttonPanel.setVisible(false);
+            }
+        });
+
+        // setting view layout
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBorder(new EmptyBorder(20, 20, 20, 20));
+
+        mouseListener = new Controller();
+
+        dealerScore = new JLabel();
+
+        dealerCardPanel = new CardPanel(this.model, this.model.getDealer());
+
+        humanScore = new JLabel();
+
+        humanCardPanel = new CardPanel(this.model, this.model.getHuman());
+
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+        buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // Hit Stay and DoubleDown buttons not visible at start
+        buttonPanel.setVisible(false);
+
+
+        hit = new JButton("Hit");
+        stay = new JButton("Stay");
+        doubleDown = new JButton("Doubledown");
+        buttonPanel.add(hit);
+        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+        buttonPanel.add(stay);
+        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+        buttonPanel.add(doubleDown);
+
+        playPanel = new JPanel();
+        playPanel.setLayout(new BoxLayout(playPanel, BoxLayout.LINE_AXIS));
+
+
+        play = new JButton("Play");
+        playPanel.add(play);
+
+
+        //adding all the Buttons and Components to the view
+        this.add(dealerScore);
+        this.add(dealerCardPanel);
+        this.add(humanScore);
+        this.add(humanCardPanel);
+        this.add(buttonPanel);
+        this.add(playPanel);
+
+
+        // adding button listeners
+        play.addActionListener(e -> model.startGame());
+
+        hit.addActionListener(e -> {
+            model.humanTurns(Turn.HIT);
+            doubleDown.setVisible(false);
+        });
+
+        stay.addActionListener(e -> {
+            model.humanTurns(Turn.STAY);
+            doubleDown.setVisible(false);
+        });
+
+        doubleDown.addActionListener((e -> {
+            try {
+                model.humanTurns(Turn.DOUBLE_DOWN);
+            } catch (Exception exc) {
+                JOptionPane.showMessageDialog(ViewPanel.this, exc.getMessage(), "Invalid", JOptionPane.ERROR_MESSAGE);
+            }
+        }));
+    }
+
+    /**
+     * Implementation of the mouse controller.
+     * Will select a field and set a stone on a mouse click event.
+     */
+    class Controller extends MouseAdapter {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            try {
+                model.dealStarterCards();
+            } catch (OutOfMoneyException e1) {
+                JOptionPane.showMessageDialog(ViewPanel.this, e1.getMessage(), "Invalid", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public void updateScore() {
+        dealerScore.setText("Dealer (" + model.getDealer().getValue() + ")");
+        humanScore.setText("Player (" + model.getHuman().getValue() + ")");
+        dealerScore.setVisible(model.getGameState() != GameState.NOTSTARTED);
+        humanScore.setVisible(model.getGameState() != GameState.NOTSTARTED);
+    }
+}
